077a0535b00ea2e80650e071992806103a20b97c
wahyuang <hello.anggara@gmail.com>
2023-06-02 09:59:48 +0800

    refactor: undo customize blog route

--- app/src/api/blog/controllers/blog.js
+++ app/src/api/blog/controllers/blog.js
@@ -6,21 +6,4 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::blog.blog", ({ strapi }) => ({
-  async findOne(ctx) {
-    const { id } = ctx.params;
-
-    const entity = await strapi.db.query("api::blog.blog").findOne({
-      where: {
-        slug: id,
-      },
-      populate: {
-        featured_image: true,
-      },
-    });
-
-    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
-
-    return this.transformResponse(sanitizedEntity);
-  },
-}));
+module.exports = createCoreController("api::blog.blog");
